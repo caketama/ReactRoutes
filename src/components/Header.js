93cb03e3ba447267aa849363010b4113a838aad6
cake <c4ke@m.me>
2019-12-03 16:24:26 -0500

    CSS progress

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
+import "../App.css"
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
-      <Link to="/first">First Component</Link>
-      <Link to="/second">Second Component</Link>
-      <Link to="/numberbutton">Number Button</Link> 
+      <Link className="link" to="/first">First Component</Link>
+      <Link className="link" to="/second">Second Component</Link>
+      <Link className="link" to="/numberbutton">Number Button</Link> 
     </div>
   );
 };
