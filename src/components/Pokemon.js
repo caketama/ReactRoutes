84cbe8908aff45dee6541cfea885282389a375c6
cake <c4ke@m.me>
2019-12-04 08:03:43 -0500

    Pokemon route

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -3,24 +3,35 @@ import React, { useState } from "react";
 function Pokemon() {
   const [data, setData] = useState({});
   const [query, setQuery] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState("");
 
   const sendData = async () => {
-    const response = await fetch(`http://pokeapi.co/api/v2/pokemon/${query}`);
-    const output = await response.json();
-    setData(output);
+    setIsLoading(true);
+    try {
+      const response = await fetch(`http://pokeapi.co/api/v2/pokemon/${query}`);
+      const output = await response.json();
+      setData(output);
+    } catch (e) {
+      setIsError(true);
+    }
+    setIsLoading(false);
   };
   return (
     // Add loading logic
     <div>
-    <input onChange={e => setQuery(e.target.value)}/>
+      <input onChange={e => setQuery(e.target.value)} />
       <button onClick={e => sendData(data)}>Search</button>
-      <div>
-      
-      <h3>{data.name}</h3>
-    {console.log(data)}
-      <p>{data.id}</p>
+      {isLoading ? (
+        <p>Loading</p>
+      ) : (
+        <div>
+          <img src={data.sprites.front_shiny} />
+          <h3>{data.name}</h3>
+          <p>{data.id}</p>
         </div>
-      </div>
-  )
+      )}
+    </div>
+  );
 }
 export default Pokemon;
